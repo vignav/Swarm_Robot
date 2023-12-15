0a6b055e9c8cc63179f674934afdac635efe2e5c
Vignav <vignav.ram@gmail.com>
2023-12-15 19:09:02 +0000

    my

---
+++ hari_servo.py
@@ -0,0 +1,39 @@
+import servo
+import math
+import server
+a=0
+while 1:
+    data=server.get_data()
+    if data[0]==0:
+        servo.servo1_set(0)
+        servo.servo2_set(0)
+    elif data[5]:
+        if(data[2]-data[4]==0):
+            continue
+        if((data[2]<data[4])and (data[1]<data[3])):
+            angle=math.atan(((data[2]-data[4])/(data[1]-data[3])))
+        elif((data[2]>data[4])and (data[1]<data[3])):
+            angle=math.atan(((data[4]-data[2])/(data[1]-data[3])))
+            # angle=math.atan(abs(data[4]-data[2])/abs(data[3]-data[1]))
+        print((math.pi/2-angle)*180/math.pi)
+        diff=(math.pi/2-angle)+data[5]*math.pi/180
+        #print(diff*180/math.pi)
+        if a==0:
+            g=1
+            if (diff>0):
+                g=g*-1
+            if(data[1]<data[3]):
+                g=g*-1
+            if(data[2]<data[4]):
+                g=g*-1
+            servo.point_dir(g)
+            if diff<10:
+                a=1
+        else:
+            servo.go_dir(diff*-1)
+            if((data[2]-data[4])**2+(data[1]-data[3])**2>100):
+               a=0
+
+
+
+
