4eafe3dd6ec4ff0007d2ab518dcfe8ba78d23c82
Vignav <vignav.ram@gmail.com>
2023-12-15 21:34:15 +0000

    blah

--- hari_servo.py
+++ hari_servo.py
@@ -2,6 +2,8 @@ import servo
 import math
 import server
 a=0
+diff=0
+angle=0
 while 1:
     data=server.get_data()
     if data[0]==0:
@@ -10,13 +12,24 @@ while 1:
     elif data[5]:
         if(data[2]-data[4]==0):
             continue
-        if((data[2]<data[4])and (data[1]<data[3])):
-            angle=math.atan(((data[2]-data[4])/(data[1]-data[3])))
-        elif((data[2]>data[4])and (data[1]<data[3])):
-            angle=math.atan(((data[4]-data[2])/(data[1]-data[3])))
-            # angle=math.atan(abs(data[4]-data[2])/abs(data[3]-data[1]))
-        print((math.pi/2-angle)*180/math.pi)
-        diff=(math.pi/2-angle)+data[5]*math.pi/180
+       # if((data[2]>data[4])and (data[1]>data[3])):
+       #     angle=math.atan(((data[2]-data[4])/(data[1]-data[3])))
+       # elif((data[2]>data[4])and (data[1]<data[3])):
+       #     angle=math.atan(((data[4]-data[2])/(data[1]-data[3])))
+        if((data[4]<data[2])and(data[3]<data[1])):
+            print("case 1")
+            angle=math.atan((data[4]-data[2])/(data[3]-data[1]))
+            diff=(math.pi/2-angle)+data[5]*math.pi/180
+        elif((data[4]<data[2])and(data[3]>data[1])):
+            print("case 2")
+            angle=math.atan((data[2]-data[4])/(data[3]-data[1]))
+            diff=math.pi/2-((math.pi/2-angle)+data[5]*math.pi/180)
+#        elif((data[4]>data[2])and(data[3]<data[1])):
+  #          print("case 3")
+   #         angle=math.atan((data[4]-data[2])/(data[3]-data[1]))
+    #        diff=((math.pi/2-angle)+data[5]*math.pi/180)
+        print(diff*180/math.pi)
+#        diff=(math.pi/2-angle)+data[5]*math.pi/180
         #print(diff*180/math.pi)
         if a==0:
             g=1
--- servo.py
+++ servo.py
@@ -1,10 +1,31 @@
 from gpiozero import Servo
 from time import sleep
 from gpiozero.pins.pigpio import PiGPIOFactory
+import math
 
 pigpio_factory = PiGPIOFactory()
 servo1 = Servo(24 , pin_factory=pigpio_factory)
 servo2 = Servo(25 , pin_factory=pigpio_factory)
+
+def servo1_set( value ):
+    if abs(value) <= 1 :
+        servo1.value = value
+    else:
+        servo1.value = 1
+
+def servo2_set( value ):
+    if abs(value) <= 1:
+        servo2.value = value*-1
+    else:
+        servo2.value = 1
+
+def go_dir ( value ):
+    servo1_set(0.5+(value*0.5/math.pi))
+    servo2_set(0.5-(value*0.5/math.pi))
+
+def point_dir ( value ):
+    servo1_set(value)
+    servo2_set(value*-1)
 
 if __name__ == '__main__' :
     try:
@@ -15,6 +36,6 @@ if __name__ == '__main__' :
                 sleep(0.1)
 
     except KeyboardInterrupt:
-        servo1.value = 0 
-        servo2.value = 0 
+        servo1.value = 0
+        servo2.value = 0
         print("Program stopped")
